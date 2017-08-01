c0de7ef293bbac6e2b77e815538324688debe725
raigh96 <30592576+raigh96@users.noreply.github.com>
2017-08-01 23:20:32 +0200

    Add files via upload

---
+++ SobrecargaDeOperadores/SobrecargaDeOperadores/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaDeOperadores
+{
+    class Person
+    {
+        private string Name;
+
+        static void Main(string[] args)
+        {
+            var pedro = new Person() { Name = "Pedro" };
+            var juan = new Person() { Name = "Pedro" };
+        }
+
+        public static Person operator == (Person a, Person b)
+        {
+            return new Person() { Name = a.Name + " " + b.Name + "son hermanos" };
+        }
+
+        public static Person operator != (Person a, Person b)
+        {
+            return new Person() { Name = a.Name + " " + b.Name + "no son hermanos" };
+        }
+
+
+    }
+}
